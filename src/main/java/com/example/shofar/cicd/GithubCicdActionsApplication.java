77d71dbc53889ae8282480a99c687e3b854df1e3
sinadn <sina.chat1398@gmail.com>
2024-08-12 14:34:41 +0330

    first commit

---
+++ src/main/java/com/example/shofar/cicd/GithubCicdActionsApplication.java
@@ -0,0 +1,31 @@
+package com.example.shofar.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionsApplication {
+
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "hello world";
+	}
+
+
+//	echo "# github-actions-sample" >> README.md
+//	git init
+//	git add README.md
+//	git commit -m "first commit"
+//	git branch -M main
+//	git remote add origin https://github.com/sinadn/github-actions-sample.git
+//	git push -u origin main
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdActionsApplication.class, args);
+	}
+
+}
