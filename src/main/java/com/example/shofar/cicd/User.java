f31437777041ab4747f87e882ef30fc7fba977b5
sinadn <sina.chat1398@gmail.com>
2024-08-21 12:27:38 +0330

    remove docker compose

---
+++ src/main/java/com/example/shofar/cicd/User.java
@@ -0,0 +1,21 @@
+package com.example.shofar.cicd;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String family;
+}
